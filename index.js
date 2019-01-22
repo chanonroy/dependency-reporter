0f99e007a8fc5c5b9f837c1828ed80332478d1bd
Chanon Roy <chanonroy@gmail.com>
2019-01-22 18:24:13 +0000

    remove license

--- index.js
+++ index.js
@@ -17,50 +17,32 @@ if (fs.existsSync(cachePath)) {
     console.log(chalk.green('Loaded previously saved dependencies file'));
 };
 
-// Get list of packages and only take values that we want
-const licenseCheckerProcess = spawn('license-report', [
-    '--only=prod',
-    '--output=json',
-    `--package=${packageJsonPath}`]
-);
+// { Dependencies Object }
+var dependencyList = JSON.parse(fs.readFileSync(packageJsonPath))['dependencies'];
 
-// To hold payload from license-report
-var newRequirements = [];
-
-licenseCheckerProcess.stdout.on('data', (data) => {
-    newRequirements = JSON.parse(data.toString());
-  });
-licenseCheckerProcess.on('close', (code) => {
-    for (var p in newRequirements) {
-        // Get values that we need from license-report
-        const package = newRequirements[p];
-        const packageName = package.name;
-        const packageVersion = package.comment;
-        const packageLink = package.link;
-
-        if (currentRequirements[packageName] === undefined) {
-            // Add to requirements list 
-            /* FORMAT
-                "react": {
-                    "versions": ["16.0.0"],
-                    "link": "git+https://github.com/facebook/react"
-                }
-            */
-           currentRequirements[packageName] = {
-                "versions": [`${packageVersion}`],
-                "link": `${packageLink}`
+for (var packageName of Object.keys(dependencyList)) {
+    const packageVersion = dependencyList[packageName]
+    if (currentRequirements[packageName] === undefined) {
+        /* FORMAT
+            "react": {
+                "versions": ["16.0.0"]
             }
-            console.log('Added ' + chalk.yellow(packageName) + ' to dependency list ...');
-        } else {
-            // Add additional version (if needed)
-            if (!currentRequirements[packageName].versions.includes(packageVersion)) {
-                currentRequirements[packageName].versions.push(packageVersion);
-                console.log('Added ' + chalk.cyan(packageVersion) + ' to version list ...');
-            }
+        */
+        currentRequirements[packageName] = {
+            "versions": [`${packageVersion}`]
+        }
+        console.log('Added ' + chalk.yellow(packageName) + ' to dependency list ...');
+    } else {
+        // Add additional version (if needed)
+        if (!currentRequirements[packageName].versions.includes(packageVersion)) {
+            currentRequirements[packageName].versions.push(packageVersion);
+            console.log('Added ' + chalk.cyan(packageVersion) + ' to version list ...');
         }
     }
-    // console.log(currentRequirements);
-    fs.writeFile(cachePath, JSON.stringify(currentRequirements), 'utf8', function(resp) {
-        console.log(chalk.green('Finished. New dependency file saved.'));
-    })
-});
+}
+
+fs.writeFile(cachePath, JSON.stringify(currentRequirements), 'utf8', function(resp) {
+    console.log(chalk.green('Finished. New dependency file saved.'));
+})
+
+return;
